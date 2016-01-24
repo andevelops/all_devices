b87fc77564691ea02900b92be0bf4340451879a3
I1PABIJJA <gamehostplay@gmail.com>
2016-01-24 11:00:19 +0500

    fix add evo_tech_3

---
+++ all_devices/fly/evo_tech_3.bat
@@ -0,0 +1,35 @@
+@echo off
+
+echo info: Start init
+
+set CD=%~dp0
+set Dini=%CD%\Dini
+set Zip=%Dini%\bin
+set LD=%Dini%\all_devices-master\all_devices
+set Dwnld=%CD%\Dwnld
+
+mkdir %Dwnld%
+
+echo info: Download main jBart and SET...
+powershell -executionpolicy bypass -file %CD%\evo_tech_3.ps1
+
+echo info: Downloading is done.
+
+echo info: Start unpack.
+
+echo info: Unpack main jBart.
+%Zip%\7z.exe x -y -r %Dwnld%\Burgerz_jBART_upd.zip -o%Dwnld%
+xcopy /Y /R /E /Q %Dwnld%\jBART-master %CD%
+
+echo info: Unpack SET for jBart.
+%Zip%\7z.exe x -y -r %Dwnld%\I1_jBART_SET_ET3_upd.zip -o%Dwnld%
+xcopy /Y /R /E /Q %Dwnld%\I1_jBart_ET3_set-master %CD%
+
+rmdir /S /Q %Dini%
+rmdir /S /Q %Dwnld%
+
+echo info: ALL DONE!
+
+del %CD%\START.bat
+del %CD%\evo_tech_3.ps1
+del %CD%\evo_tech_3.bat
---
+++ all_devices/fly/evo_tech_3.ps1
@@ -0,0 +1,8 @@
+$curDir = $MyInvocation.MyCommand.Definition | split-path -parent
+$Url = "https://github.com/BurgerZ/jBART/archive/master.zip"
+$Path = "$curDir\Dwnld\Burgerz_jBART_upd.zip" 
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($url,$path)
+$Url2 = "https://github.com/MIUI-v7-porters/I1_jBart_ET3_set/archive/master.zip"
+$Path2 = "$curDir\Dwnld\I1_jBART_SET_ET3_upd.zip" 
+$WebClient.DownloadFile($url2,$path2)
